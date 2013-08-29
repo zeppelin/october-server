2329356e821a08411da6cc79e883e95ecf6028fc
Toth David <tothda@gmail.com>
2013-08-29 16:29:40 +0200

    Issue title from ...

--- app/controllers/pomodoros_controller.rb
+++ app/controllers/pomodoros_controller.rb
@@ -4,7 +4,11 @@ class PomodorosController < ApplicationController
     github_issue_id = issue_params[:github_issue_id]
     owner = issue_params[:owner]
     repo = issue_params[:repo]
-    issue = Issue.where(github_issue_id: github_issue_id, owner: owner, repo: repo).first || Issue.new(issue_params)
+    issue = Issue.where(github_issue_id: github_issue_id, owner: owner, repo: repo).first
+    unless issue
+      title = get_title_from_github(owner, repo, github_issue_id)
+      issue = Issue.new(issue_params.merge(title: title))
+    end
     pomodoro = issue.pomodoros.build(pomodoro_params)
 
     if issue.save
@@ -28,6 +32,10 @@ class PomodorosController < ApplicationController
 
     def issue_params
       params.require(:pomodoro).require(:issue).permit(:owner, :repo, :github_issue_id)
+    end
+
+    def get_title_from_github(owner, repo, github_issue_id)
+      
     end
 
 end
