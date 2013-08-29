c781a4a26e78dd1b99ec2e9ea3a9828e39f6133b
Balint Erdi <balint.erdi@gmail.com>
2013-08-29 11:02:06 +0200

    Set up Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OctoberServer::Application.config.secret_key_base = 'cd0cc2b296b2541708f44df02a6c88132c1b0ed07637826ac980b94a3f9e5e495d2ad21a5fbe2f4dc8de64045d4056b6215998b8470a93f435430b69606c207b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OctoberServer::Application.config.session_store :cookie_store, key: '_october-server_session'
